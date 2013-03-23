b8c8839265c5c05b9f7ddd9c8f1e60f33ba1ae15
xioTechnologies <social@x-io.co.uk>
2013-03-23 18:38:45 +0000

    initial commit

---
+++ x-OSC Firmware Uploader/x-OSC Firmware Uploader/FormMain.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace x_OSC_Firmware_Uploader
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.labelHexFile = new System.Windows.Forms.Label();
+            this.textBoxHexFile = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.labelSerialPort = new System.Windows.Forms.Label();
+            this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(140, 342);
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Enabled = false;
+            this.buttonUpload.Location = new System.Drawing.Point(407, 307);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpload.TabIndex = 3;
+            this.buttonUpload.Text = "Upload";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.button_upload_Click);
+            // 
+            // labelHexFile
+            // 
+            this.labelHexFile.AutoSize = true;
+            this.labelHexFile.Location = new System.Drawing.Point(146, 178);
+            this.labelHexFile.Name = "labelHexFile";
+            this.labelHexFile.Size = new System.Drawing.Size(48, 13);
+            this.labelHexFile.TabIndex = 9;
+            this.labelHexFile.Text = "Hex File:";
+            // 
+            // textBoxHexFile
+            // 
+            this.textBoxHexFile.Location = new System.Drawing.Point(210, 175);
+            this.textBoxHexFile.Name = "textBoxHexFile";
+            this.textBoxHexFile.Size = new System.Drawing.Size(241, 20);
+            this.textBoxHexFile.TabIndex = 10;
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(457, 173);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(25, 23);
+            this.buttonBrowse.TabIndex = 11;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.button_browse_Click);
+            // 
+            // labelSerialPort
+            // 
+            this.labelSerialPort.AutoSize = true;
+            this.labelSerialPort.Location = new System.Drawing.Point(146, 133);
+            this.labelSerialPort.Name = "labelSerialPort";
+            this.labelSerialPort.Size = new System.Drawing.Size(58, 13);
+            this.labelSerialPort.TabIndex = 12;
+            this.labelSerialPort.Text = "Serial Port:";
+            // 
+            // comboBoxSerialPort
+            // 
+            this.comboBoxSerialPort.FormattingEnabled = true;
+            this.comboBoxSerialPort.Location = new System.Drawing.Point(210, 130);
+            this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+            this.comboBoxSerialPort.Size = new System.Drawing.Size(75, 21);
+            this.comboBoxSerialPort.TabIndex = 13;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 342);
+            this.Controls.Add(this.comboBoxSerialPort);
+            this.Controls.Add(this.labelSerialPort);
+            this.Controls.Add(this.labelHexFile);
+            this.Controls.Add(this.textBoxHexFile);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.buttonUpload);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.Label labelHexFile;
+        private System.Windows.Forms.TextBox textBoxHexFile;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Label labelSerialPort;
+        private System.Windows.Forms.ComboBox comboBoxSerialPort;
+    }
+}
+
---
+++ x-OSC Firmware Uploader/x-OSC Firmware Uploader/FormMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.IO.Ports;
+using System.IO;
+using System.Diagnostics;
+
+namespace x_OSC_Firmware_Uploader
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.Text = Assembly.GetExecutingAssembly().GetName().Name + " " + Assembly.GetExecutingAssembly().GetName().Version.Major.ToString() + "." + Assembly.GetExecutingAssembly().GetName().Version.Minor.ToString();
+            foreach (string portName in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                comboBoxSerialPort.Items.Add(portName);
+            }
+            comboBoxSerialPort.SelectedIndex = 0;
+        }
+
+        private void button_browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select Firmware Hex File";
+            openFileDialog.Filter = "Hex Files|*.hex";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxHexFile.Text = openFileDialog.FileName.ToString();
+            }
+            buttonUpload.Enabled = File.Exists(textBoxHexFile.Text);
+        }
+
+        private void button_upload_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo processInfo = new ProcessStartInfo("ds30LoaderConsole.exe");
+            processInfo.Arguments = "\"-f=" + textBoxHexFile.Text + "\"" +
+                                    " -d=dsPIC33EP512MC806 " +
+                                    "\"-k=" + comboBoxSerialPort.Text + "\"" +
+                                    " -r=115200 --writef --ht=30000 --polltime=100 --timeout=3000 -o";
+            processInfo.UseShellExecute = false;
+            Process process = Process.Start(processInfo);
+            process.WaitForExit();
+            process.Close();
+            this.Close();
+        }
+    }
+}
